ac85f110798fcabf8785d331af0b789c53707d0f
Prince Panjiyar <91174979+Princep02@users.noreply.github.com>
2023-11-05 14:52:20 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,24 +1,24 @@
-N=9
+N=9  #declare variable hold num of rows or cols
 
-def isSafe(sudoku, row, col, num):
+def isSafe(sudoku, row, col, num):  #fuction to check is it ok to assign a num in given rows and cols
     for i in range(9):
-        if sudoku[row][i]==num:
+        if sudoku[row][i]==num:  #check in rows
             return False
 
     for i in range(9):
-        if sudoku[i][col]==num:
+        if sudoku[i][col]==num:  #check in cols
             return False
 
-    startRow=row-row%3
+    startRow=row-row%3  #chech same num exists in 3*3 grid
     startCol=col-col%3
     for i in range(3):
         for j in range(3):
             if sudoku[startRow+i][startCol+j]==num:
                 return False
-    return True
+    return True  #executes if none of above condition is satsified
 
-def solveSudoku(sudoku, row, col):
-    if row==N-1 and col==N:
+def solveSudoku(sudoku, row, col):   #function to solve sudoku
+    if row==N-1 and col==N:  #base conditon since we use recursion
         return True
 
     if col==N:
@@ -29,19 +29,19 @@ def solveSudoku(sudoku, row, col):
         return solveSudoku(sudoku, row, col+1)
 
     for num in range(1,N+1):
-        if isSafe(sudoku, row, col, num):
+        if isSafe(sudoku, row, col, num): #if ok to assign num we assign in sudoku
             sudoku[row][col]=num
 
-            if solveSudoku(sudoku,row,col+1):
+            if solveSudoku(sudoku,row,col+1): #check for possibility with next column
                 return True
 
 
 
-        sudoku[row][col]=0
+        sudoku[row][col]=0 #reassign 0 at given position since our assumption was wrong
     return False
 
-def solver(sudoku):
-  if solveSudoku(sudoku, 0, 0):
+def solver(sudoku):  #function to solve sudoku if solvable
+  if solveSudoku(sudoku, 0, 0):  #take sudoku as arguement and solve starting form 0,0
       return sudoku
   else:
       return"no"
